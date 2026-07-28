1877d13e8208d9d405bc98992bdf7a394117a146
Sinchana41 <sinchanaan1234@gmail.com>
2026-07-28 11:11:43 +0530

    Initial commit

---
+++ src/main/java/com/bl/trainconsistmanagementapp/TrainConsistManagementApp.java
@@ -0,0 +1,8 @@
+package com.bl.trainconsistmanagementapp;
+
+public class TrainConsistManagementApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Train Consist Management App");
+    }
+}
