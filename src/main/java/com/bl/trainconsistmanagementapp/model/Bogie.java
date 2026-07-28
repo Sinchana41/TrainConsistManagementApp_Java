e169becaeb5892e1109b6fed45a4b41a045179f1
Sinchana41 <sinchanaan1234@gmail.com>
2026-07-28 14:35:44 +0530

    Add Passenger Bogies to Train

--- src/main/java/com/bl/trainconsistmanagementapp/model/Bogie.java
+++ src/main/java/com/bl/trainconsistmanagementapp/model/Bogie.java
@@ -3,12 +3,26 @@ package com.bl.trainconsistmanagementapp.model;
 public class Bogie {
 
     private String bogieId;
+    private String bogieType;
 
-    public Bogie(String bogieId) {
+    public Bogie(String bogieId, String bogieType) {
         this.bogieId = bogieId;
+        this.bogieType = bogieType;
     }
 
     public String getBogieId() {
         return bogieId;
+    }
+
+    public String getBogieType() {
+        return bogieType;
+    }
+
+    @Override
+    public String toString() {
+        return "Bogie{" +
+                "bogieId='" + bogieId + '\'' +
+                ", bogieType='" + bogieType + '\'' +
+                '}';
     }
 }
--- src/main/java/com/bl/trainconsistmanagementapp/service/TrainConsist.java
+++ src/main/java/com/bl/trainconsistmanagementapp/service/TrainConsist.java
@@ -13,7 +13,23 @@ public class TrainConsist {
         this.bogies = new ArrayList<>();
     }
 
-    public int getBogieCount(){
-       return bogies.size();
+    public int getBogieCount() {
+        return bogies.size();
+    }
+
+    public void addBogie(Bogie bogie) {
+        bogies.add(bogie);
+        System.out.println("Attached: " + bogie.getBogieId());
+    }
+
+    public void displayConsistDetails() {
+        System.out.println("\n--- Current Train Consist Details ---");
+        if (bogies.isEmpty()) {
+            System.out.println("No bogies attached.");
+        } else {
+            for (int i = 0; i < bogies.size(); i++) {
+                System.out.printf("Position %d: %s%n", i + 1, bogies.get(i));
+            }
+        }
     }
 }
